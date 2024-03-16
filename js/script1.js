65575d57649f14df9d212b45c6595b85c87ef66c
Don-Juan182 <thegoodmarmage@hotmail.com>
2024-03-16 19:54:59 +0100

    Created an new repository for lesson 4

---
+++ js/script1.js
@@ -0,0 +1,24 @@
+/*MODULE 2-lesson 4*/
+/*Question 1*/
+function logPhrase() {
+    console.log("What the func is this nonsense?");
+}
+
+logPhrase();
+
+/*Question 2*/
+
+
+/*Question 3*/
+
+
+/*Question 4*/
+
+
+/*Question 5*/
+
+
+/*Question 6*/
+
+
+/*Question 7*/
